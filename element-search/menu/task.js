8d15a77fcf8e45833817cc3dea3a3464aec3572a
tpecherkina <74115313+tpecherkina@users.noreply.github.com>
2021-10-07 23:52:15 -0400

    Update task.js

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,10 +1,11 @@
-   var menuLink = document.querySelectorAll(".menu__link");
-   var menuSub = document.querySelectorAll(".menu_sub");
-
-for(let i = 0; i < menuLink.length; i++) {
-    var menuFirst = menuSub[i].closest(".menu__item");
-    menuFirst.onclick = function() {
-      menuSub[i].classList.toggle("menu_active");
-      return false;
-    }
-    }
+  let menuLink = document.querySelectorAll(".menu__link");
+   let menuSub = document.querySelectorAll(".menu_sub");
+   
+   
+   for (let item of menuSub) {
+const menuClicker = item.parentElement;
+menuClicker.onclick = function() {
+  item.classList.toggle("menu_active");
+  return false;
+}
+   }
