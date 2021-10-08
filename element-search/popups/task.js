671e87310b3b8dbe9d95049b61326199f65e6daa
tpecherkina <74115313+tpecherkina@users.noreply.github.com>
2021-10-07 23:53:16 -0400

    Update task.js

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,8 +1,8 @@
-var modalMain = document.getElementById("modal_main");
-var modalSuccess = document.getElementById("modal_success");
-var showSuccess = document.getElementsByClassName("show-success");
-var closeWindow = document.getElementById("modal__close_times")
-var modal = document.getElementsByClassName("modal");
+let modalMain = document.getElementById("modal_main");
+let modalSuccess = document.getElementById("modal_success");
+let showSuccess = document.getElementsByClassName("show-success");
+let closeWindow = document.getElementsByClassName("modal__close_times")
+let modal = document.getElementsByClassName("modal");
 
 modalMain.classList.add("modal_active");
 
@@ -12,7 +12,9 @@ modalMain.classList.add("modal_active");
 
     }
 
-    closeWindow[0].onclick = function() {
-        modalSuccess.classList.toggle("modal_active");
-    }
 
+    for(let i = 0; i < closeWindow.length; i++) {
+        closeWindow[i].onclick = function() {
+            modal[i].classList.remove("modal_active");
+        }
+    }
