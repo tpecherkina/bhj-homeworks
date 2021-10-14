a5734b8b5c17db380637c7e529bef9e6832600c7
tpecherkina <74115313+tpecherkina@users.noreply.github.com>
2021-10-13 21:08:15 -0400

    Update task.js

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,11 +1,12 @@
-  let menuLink = document.querySelectorAll(".menu__link");
-   let menuSub = document.querySelectorAll(".menu_sub");
-   
-   
+let menuSub = document.querySelectorAll(".menu_sub");
+
+  
    for (let item of menuSub) {
 const menuClicker = item.parentElement;
-menuClicker.onclick = function() {
+const link = menuClicker.querySelector('a');
+link.onclick = function() {
   item.classList.toggle("menu_active");
   return false;
 }
-   }
+}
+
