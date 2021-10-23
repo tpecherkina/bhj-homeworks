0180c17e597fd878b71b52bfd75e525546963b07
tpecherkina <74115313+tpecherkina@users.noreply.github.com>
2021-10-23 19:55:46 -0400

    Updated Tabs

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,18 +1,14 @@
 let tab = document.querySelectorAll(".tab");
-
-
 let tabs = Array.from(tab);
 let tabContent = document.querySelectorAll(".tab__content");
 
-tabs.onclick = function() {
-  tabs.classList.remove("tab__content_active");
-  this.classList.add("tab_active");
-}
 
-for (let i = 0; i < tabs.length; i++) {
-  tabs[i].onclick = function() {
-    tabContent[i].classList.toggle("tab__content_active");
-    return false;
-  }
-
+for(let i = 0; i < tabs.length; i++) {
+ tabs[i].onclick = function() {
+    let searchActive = tabs.findIndex((link) => link.classList.contains("tab_active"));
+     tabs[searchActive].classList.remove("tab_active");
+     tabContent[searchActive].classList.remove("tab__content_active");
+     tabs[i].classList.add("tab_active");
+     tabContent[i].classList.add("tab__content_active");
+ }
 }
