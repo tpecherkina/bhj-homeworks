193f808857065c7eaa7c461a599ce6c5c11aad8b
tpecherkina <74115313+tpecherkina@users.noreply.github.com>
2021-11-24 19:34:00 -0500

    Update task.js

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -7,13 +7,18 @@ toolTip.classList.add("tooltip");
 toolTipTarget.forEach(item => {
 	item.addEventListener('click', (e) => {
 		e.preventDefault();
-		
-		
+		if (item.title === toolTip.innerText) {
+			toolTip.classList.remove('tooltip_active');
+			
+			
+		}
+		 
 		toolTip.innerText = item.title;
 		item.insertAdjacentElement('afterEnd', toolTip);
 	        let topBorder = item.getBoundingClientRect().bottom;
 		let leftBorder = item.getBoundingClientRect().left;
 		toolTip.setAttribute('style', `top: ${topBorder}px; left: ${leftBorder}px`)
-		toolTip.classList.toggle("tooltip_active");
-	})
+		toolTip.classList.add("tooltip_active");
+	
+})
 });
